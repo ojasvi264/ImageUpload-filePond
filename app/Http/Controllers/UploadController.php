71df60d708ba9f5d48e7d4b2a4ecb79edb56d530
ojasvi264 <lalamdorje@gmail.com>
2024-11-25 18:49:13 +0900

    adds UploadController to store image in filePond session.

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TemporaryFile;
+use Illuminate\Http\Request;
+
+class UploadController extends Controller
+{
+    public function store(Request $request){
+        if($request->hasFile('image')){
+            $file = $request->file('image');
+            $filename = $file->getClientOriginalName();
+            $folder = uniqid(). '-' . now()->timestamp;
+            $file->storeAs('images/tmp' . $folder, $filename);
+            TemporaryFile::create([
+                'folder' => $folder,
+                'filename' => $filename,
+            ]);
+
+            return $folder;
+        }
+        return '';
+    }
+}
