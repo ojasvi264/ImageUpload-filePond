74951f09988fd8c3b123361438f518d870e6d52f
ojasvi264 <lalamdorje@gmail.com>
2024-11-25 18:47:55 +0900

    adds create image file UI.

---
+++ resources/views/image_upload/create.blade.php
@@ -0,0 +1,48 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <form method="POST" action="{{ route('image.store') }}">
+                        @csrf
+                        <!-- Single Image Upload -->
+                        <div>
+                            <x-input-label for="image" :value="__('Image')" />
+                            <x-file-input id="image" class="block mt-1 w-full" type="file" name="image" :value="old('image')" required autofocus autocomplete="Upload an Image." />
+                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                        </div>
+
+                        <div class="flex items-center justify-end mt-4">
+                            <x-primary-button class="ms-3">
+                                {{ __('Upload') }}
+                            </x-primary-button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    @section('scripts')
+        <script>
+            // Get a reference to the file input element
+            const inputElement = document.querySelector('input[id="image"]');
+
+            // Create a FilePond instance
+            const pond = FilePond.create(inputElement);
+            FilePond.setOptions({
+                server: {
+                    url: '/upload',
+                    headers: {
+                        'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                    }
+                }
+            });
+        </script>
+    @endsection
+</x-app-layout>
