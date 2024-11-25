99f3055bfb080d44225a9a3ccb040d97f3646740
ojasvi264 <lalamdorje@gmail.com>
2024-11-25 18:46:30 +0900

    adds image migration and model.

---
+++ app/Models/Image.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Image extends Model implements HasMedia
+{
+    use InteractsWithMedia;
+    protected $fillable = ['image'];
+
+    public function getImageAttribute(){
+        return $this->getMedia()->first()?->getFullUrl();
+    }
+}
