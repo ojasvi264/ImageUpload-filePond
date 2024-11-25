ff3751a501244238e6c75894d86c4303e4980351
ojasvi264 <lalamdorje@gmail.com>
2024-11-25 18:48:11 +0900

    adds listing image file.

---
+++ resources/views/image_upload/list.blade.php
@@ -0,0 +1,41 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        @if (session('success'))
+            <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+                <span class="block sm:inline">{{ session('success') }}</span>
+            </div>
+        @endif
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <table class="table-auto">
+                        <thead>
+                            <tr>
+                                <th>S.N</th>
+                                <th>Image</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($imageList as $index => $image)
+{{--                                @dd(asset($image->image))--}}
+                                <tr>
+                                    <td>{{ ++$index }}</td>
+{{--                                    <td>{{ $image->image }}</td>--}}
+                                    <td>
+                                        <img src="{{asset($image->image)}}" alt="" height="100px" width="100px">
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
