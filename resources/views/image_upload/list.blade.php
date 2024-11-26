ae5dfe0bed9f9cd04f317c60dabd62f0b9344cea
ojasvi264 <lalamdorje@gmail.com>
2024-11-26 15:45:25 +0900

    adds create button on list page.

--- resources/views/image_upload/list.blade.php
+++ resources/views/image_upload/list.blade.php
@@ -12,6 +12,11 @@
             </div>
         @endif
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <a href="{{ route('image.create') }}">
+                <x-primary-button class="ms-3 float-right">
+                    {{ __('Create') }}
+                </x-primary-button>
+            </a>
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <table class="table-auto">
