fad3ac727c530c4a83387e9c0e4fc918d4adfec0
ojasvi264 <lalamdorje@gmail.com>
2024-11-28 18:21:09 +0900

    adds multiple image migration and model

---
+++ app/Models/MultipleImage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class MultipleImage extends Model implements HasMedia
+{
+    use InteractsWithMedia;
+    protected $fillable = ['photo'];
+}
