b150a85adade2f561e562893dcf6027d15c96581
ojasvi264 <lalamdorje@gmail.com>
2024-11-28 18:23:14 +0900

    adds route for multiple image upload.

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,13 @@ Route::middleware('auth')->group(function () {
 Route::get('/image/create', [\App\Http\Controllers\ImageUploadController::class, 'create'])->name('image.create');
 Route::get('/image/list', [\App\Http\Controllers\ImageUploadController::class, 'list'])->name('image.list');
 Route::post('/image/store', [\App\Http\Controllers\ImageUploadController::class, 'store'])->name('image.store');
+
+Route::get('/multiple-image/create', [\App\Http\Controllers\MultipleImageUploadController::class, 'create'])->name('multiple_image.create');
+Route::get('/multiple-image/list', [\App\Http\Controllers\MultipleImageUploadController::class, 'list'])->name('multiple_image.list');
+
+Route::post('/multiple-image/store', [\App\Http\Controllers\MultipleImageUploadController::class, 'multipleStore'])->name('multiple_image.store');
+
 Route::post('/upload', [\App\Http\Controllers\UploadController::class, 'store'])->name('image.upload');
+Route::delete('/upload', [\App\Http\Controllers\UploadController::class, 'destroy'])->name('image.delete');
+
 require __DIR__.'/auth.php';
