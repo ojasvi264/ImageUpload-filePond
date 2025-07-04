49531d927c1e2a28339d8dccb9d8d4db21f759eb
ojasvi264 <lalamdorje@gmail.com>
2025-07-04 15:39:10 +0900

    updates routes inside the middleware.

--- routes/web.php
+++ routes/web.php
@@ -18,19 +18,19 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/image/create', [ImageUploadController::class, 'create'])->name('image.create');
+    Route::get('/image/list', [ImageUploadController::class, 'list'])->name('image.list');
+    Route::post('/image/store', [ImageUploadController::class, 'store'])->name('image.store');
+    Route::delete('/image/{image}/destroy', [ImageUploadController::class, 'destroy'])->name('image.destroy');
+
+    Route::get('/multiple-image/create', [MultipleImageUploadController::class, 'create'])->name('multiple_image.create');
+    Route::get('/multiple-image/list', [MultipleImageUploadController::class, 'list'])->name('multiple_image.list');
+
+    Route::post('/multiple-image/store', [MultipleImageUploadController::class, 'multipleStore'])->name('multiple_image.store');
+
+    Route::post('/upload', [UploadController::class, 'store'])->name('image.upload');
+    Route::delete('/upload', [UploadController::class, 'destroy'])->name('image.delete');
 });
-Route::get('/image/create', [ImageUploadController::class, 'create'])->name('image.create');
-Route::get('/image/list', [ImageUploadController::class, 'list'])->name('image.list');
-Route::post('/image/store', [ImageUploadController::class, 'store'])->name('image.store');
-Route::delete('/image/{image}/destroy', [ImageUploadController::class, 'destroy'])->name('image.destroy');
-
-Route::get('/multiple-image/create', [MultipleImageUploadController::class, 'create'])->name('multiple_image.create');
-Route::get('/multiple-image/list', [MultipleImageUploadController::class, 'list'])->name('multiple_image.list');
-
-Route::post('/multiple-image/store', [MultipleImageUploadController::class, 'multipleStore'])->name('multiple_image.store');
-
-Route::post('/upload', [UploadController::class, 'store'])->name('image.upload');
-Route::delete('/upload', [UploadController::class, 'destroy'])->name('image.delete');
-
 
 require __DIR__.'/auth.php';
